ec1a00b930074cbfff1ed87dcc685b4837f19117
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-05 23:58:23 -0600

    fix(Navbar): change route of logo if is Authenticated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,12 @@ const Navbar = () => {
         // <nav className="bg-zinc-700 flex justify-between py-5 px-10 rounded-b-xl">
         // <nav className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20 rounded-b-xl">
         <nav className="bg-transparent h-[100px] gap-3 flex flex-col sm:flex-row items-center justify-between rounded-b-xl px-4">
-            <Link href="/" className="mt-2 sm:mt-0 text-xl text-white inline-block text-[25px] font-semibold sm:text-3xl">
+            <Link 
+                href={
+                    isAuthenticated ? "/loans" : "/"
+                } 
+                className="mt-2 sm:mt-0 text-xl text-white inline-block text-[25px] font-semibold sm:text-3xl"
+            >
                 Administrador de 
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-700">
                     &nbsp;Prestamos
